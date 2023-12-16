3d50301e4d12eecdee7e48bc194f89154a55e6ab
HASHEBUR RAHMAN <mdhasheburrahman3@gmail.com>
2023-12-16 21:30:45 +0600

    test

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import colors from 'colors';
-import dotenv from 'dotenv';
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
 import morgan from "morgan";
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoute.js';
-import categoryRoutes from './routes/categoryRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import bannerRoutes from './routes/bannerRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import latestproductRoutes from './routes/latestproductRoutes.js'
-import cors from 'cors';
-import path from 'path';
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import bannerRoutes from "./routes/bannerRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import latestproductRoutes from "./routes/latestproductRoutes.js";
+import cors from "cors";
+import path from "path";
 
 //configure env
 dotenv.config();
@@ -24,16 +24,21 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 // app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
-app.use('/api/v1/auth',authRoutes);
-app.use('/api/v1/category',categoryRoutes);
-app.use('/api/v1/product',productRoutes);
-app.use('/api/v1/banner',bannerRoutes);
-app.use('/api/v1/order',orderRoutes);
-app.use('/api/v1/latestproduct',latestproductRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/banner", bannerRoutes);
+app.use("/api/v1/order", orderRoutes);
+app.use("/api/v1/latestproduct", latestproductRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello, Vercel!");
+});
+
 
 // rest api
 // app.use('*',function(req,res){
@@ -43,13 +48,10 @@ app.use('/api/v1/latestproduct',latestproductRoutes);
 //PORT
 const PORT = process.env.PORT || 7000;
 
-
-
-
-
-
 //run listen
-app.listen(PORT, ()=>{
-    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgGreen.black);
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgGreen
+      .black
+  );
 });
-
